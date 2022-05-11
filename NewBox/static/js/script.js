b7463aea998e2f5d80ed47bb61d7bf4176d5cff4
VictorP03 <crapowner321@gmail.com>
2022-05-11 20:15:06 +0200

    Moved and renamed jquery file. Navbar is now expandable (no backend yet).

---
+++ NewBox/static/js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(".navbar-search").click(function(){
+        $(this).hide();
+        $(".top-navbar h1, .navbar-search-settings").hide();
+        $(".searchbar").focus().toggleClass('full-width').toggleClass('flex').val('');
+        setTimeout(function (){
+            $('#navbar-search').toggleClass('full-width').toggleClass('flex').val('');
+            $('.navbar-search-small').show();
+            $(".navbar-cross-small").show();
+        }, 500);
+    });
+
+    $(".navbar-cross-small").click(function(){
+        $(this).hide();
+        $('.navbar-search-small').hide();
+        $(".navbar-cross-small").hide();
+        $('#navbar-search').focus().toggleClass('full-width').toggleClass('flex').val('');
+        $(".searchbar").focus().toggleClass("full-width").toggleClass("flex").val('');
+        setTimeout(function (){
+            $(".navbar-search").show();
+            $(".top-navbar h1, .navbar-search-settings").show();
+        }, 500);
+    });
+});
