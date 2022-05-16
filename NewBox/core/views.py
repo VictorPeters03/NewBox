fcd7c564bd0b8be3de3f7234a3c6599df5efa1ad
VictorP03 <crapowner321@gmail.com>
2022-05-16 15:15:55 +0200

    Added urls and template for javascript testing.

--- NewBox/core/views.py
+++ NewBox/core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def download(request):
+    return render(request, 'download.html')
