f8efb8cc5e3e9ac4cf7ca4fcbcd611e92e253b5b
RonaldNed <ronald.nederveen@student.nhlstenden.com>
2022-05-17 13:28:01 +0200

    added all endpoints

--- NewBoxAPI/main.py
+++ NewBoxAPI/main.py
@@ -16,3 +16,28 @@ async def set_max_volume(amount: int):
 @app.put("/beheer/minvolume/{amount}")
 async def set_minM_volume(amount: int):
     return
+
+#endpoint voor het toevoegen aan wachtrij
+@app.put("/use/queue/{id}")
+async def add_to_queue(id: str):
+    return
+
+#endpoint voor het ophalen van de wachtrij
+@app.get("/use/getqueue")
+async def get_queue():
+    return
+
+#endpoint voor afspelen muziek
+@app.put("/use/play/{id}")
+async def play_music(id: str):
+    return
+
+#endpoint voor het pauzeren van spelende muziek
+@app.put("/use/toggleplay")
+async def toggle_music():
+    return
+
+#endpoint voor het zoeken van muziek
+@app.get("/use/search/{key}")
+async def search_music(key: str):
+    return
