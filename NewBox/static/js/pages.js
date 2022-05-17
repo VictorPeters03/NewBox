1f77716dafb988404f7e1aa7935a5e6aabc37d4b
VictorP03 <crapowner321@gmail.com>
2022-05-17 12:27:54 +0200

    Axios get request displays in html.

--- NewBox/static/js/pages.js
+++ NewBox/static/js/pages.js
@@ -4,6 +4,11 @@ const downloaded = document.querySelector(".button-downloaded")
 
 const index = document.querySelector("#sub-container")
 
+function getHomePage()
+{
+    index.innerHTML = ""
+}
+
 function getSongs()
 {
     index.innerHTML = '<div id="songs-downloaded">\n' +
@@ -18,14 +23,18 @@ function getSongs()
 
 function getDownloaded()
 {
-    index.innerHTML = '<div id="songs-downloaded">\n' +
-            '            <div class="song-downloaded">\n' +
-            '                <h1>Song 1</h1>\n' +
-            '            </div>\n' +
-            '            <div class="song-downloaded">\n' +
-            '                <h1>Song 2</h1>\n' +
-            '            </div>\n' +
-            '        </div>'
+    index.innerHTML = '<div id="songs-downloaded"></div>\n'
+
+    let songs_downloaded = document.querySelector('#songs-downloaded')
+
+    axios.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+    .then(resp => {
+        resp.data.data.forEach(element =>
+            songs_downloaded.innerHTML += '<div class="song-downloaded">\n' +
+        '                       <h1>' + element["Nation"] + '</h1>\n' +
+        '                   </div>\n'
+        )
+    });
 }
 
 songs.addEventListener("click", getSongs)
