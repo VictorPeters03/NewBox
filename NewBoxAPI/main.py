12ddd098d420176d0614d903b5199fc1015cf2cf
RonaldNed <ronald.nederveen@student.nhlstenden.com>
2022-05-17 10:34:55 +0200

    setup internal API directory

---
+++ NewBoxAPI/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.get("/beheer/")
+async def 
