9e6400c1fb65cd844e2600d55e5a8a0dc5cacf40
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2022-05-18 15:47:43 +0200

    added shuffle, addtoqueue and setrepeat methods and changed some syntax

--- NewBox/spotifyAPI/functions.py
+++ NewBox/spotifyAPI/functions.py
@@ -1,5 +1,7 @@
+import spotipy
 from authorization import spotifyHandler
 from secrets import DEVICE_ID
+
 
 # User related functions
 def getUserDetails():
@@ -29,26 +31,28 @@ def searchFor(resultSize, searchQuery, returnType='tracks'):
     search = spotifyHandler.search(searchQuery, type=returnType)
 
     for i in range(0, resultSize):
-        songInfo = {}
-        songId = search[returnType + 's']['items'][i]['id']
-        songName = search[returnType + 's']['items'][i]['name']
+        itemInfo = {}
+        id = search[returnType + 's']['items'][i]['id']
+        name = search[returnType + 's']['items'][i]['name']
         uri = search[returnType + 's']['items'][i]['uri']
-        # artistName = search[returnType + 's']['items'][i]['artists'][0]['name']
 
-        songInfo.update({'nr': i + 1, 'type': returnType, 'songId': songId, 'songName': songName, 'uri': uri})
-        items.append(songInfo)
+        itemInfo.update({'nr': i + 1,
+                         'type': returnType,
+                         'id': id,
+                         'name': name,
+                         'uri': uri})
+        items.append(itemInfo)
 
     for i in range(len(items)):
         print(items[i])
     return items
 
 
-def selectFromSearch(items):
+def selectFromSearch(items, number):
     for i in range(len(items)):
         for j in items[i]:
-            if items[i].get(j) == choice:
-                play(items[i].get('uri'))
-
+            if items[i].get(j) == number:
+                return items[i].get('uri')
 
 
 # Playlist related functions
@@ -85,19 +89,38 @@ def resume():
 
 
 def previous():
-    # functie geeft nog error
+    # function gives an error, needs fix
     try:
         spotifyHandler.previous_track(device_id=DEVICE_ID)
-    except spotipy.exceptions.SpotifyException as e:
+    except spotipy.exceptions.SpotifyException:
         return
 
 
-# set volume between 0 - 100%
 def setVolume(volume):
+    # set volume between 0 - 100%
     spotifyHandler.volume(device_id=DEVICE_ID, volume_percent=volume)
 
-# inp = input("Search for a song, playlist, album etc: ")
-# searchResult = searchFor(5, inp, returnType='playlist')
-# choice = int(input("Enter number: "))
-# selectFromSearch(searchResult)
-# setVolume(0)
+
+def shuffle(state):
+    # state can either be true or false
+    spotifyHandler.shuffle(device_id=DEVICE_ID, state=state)
+
+
+def addToQueue(uri):
+    if 'track' in uri:
+        spotifyHandler.add_to_queue(device_id=DEVICE_ID, uri=uri)
+    else:
+        print('cannot add playlist or album to queue')
+        return
+
+
+def setRepeat(state):
+    # state can either be track, context or off
+    spotifyHandler.repeat(state=state, device_id=DEVICE_ID)
+
+
+inp = input("Search for a song, playlist, album etc: ")
+searchResult = searchFor(5, inp, returnType='playlist')
+choice = int(input("Enter number: "))
+playThisUri = selectFromSearch(searchResult, choice)
+play(playThisUri)
