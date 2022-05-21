c6842f9f3a722785d5cb0db61cea006dc17436de
VictorP03 <crapowner321@gmail.com>
2022-05-21 19:35:45 +0200

    Conducted tests with Spotify API. Can load in different songs from specified playlists.

--- NewBox/static/js/pages.js
+++ NewBox/static/js/pages.js
@@ -1,3 +1,9 @@
+// import axios from "axios";
+
+const CLIENT_ID = "ec41429613ba45c8984f3119527186ff"
+
+const CLIENT_SECRET = "62547ecfa262441bb6789d1004b45e82"
+
 const songs = document.querySelector(".button-songs")
 
 const downloaded = document.querySelector(".button-downloaded")
@@ -17,11 +23,24 @@ function getSongs()
 
     let songs_downloaded = document.querySelector('#songs-downloaded')
 
-    axios.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+    axios.get("https://api.spotify.com/v1/playlists/37i9dQZF1DX2LTcinqsO68/tracks?market=NL&limit=20",
+        {
+            headers:
+            {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer BQAhBFQeyIxdEqwRO7FIr1MnJFhN0aFTzeLGOZr9u2nJUwGgoxefGhW3vYtP775tXYgN3ispW2c0tUX8rftlnYPkWwOK5SLVKflPPz5IgCHCZ1MyuDMASP53u6xglDjZYZNPx4xY5KY-blIVHJSimv_CDcQND0v6NZg"
+            }
+        })
     .then(resp => {
-        resp.data.data.forEach(element =>
-            songs_downloaded.innerHTML += '<div class="song-downloaded">\n' +
-        '                       <h1>' + element["Year"] + '</h1>\n' +
+        resp.data.items.forEach(element =>
+            // console.log(element["track"]["artists"][0]["name"])
+        songs_downloaded.innerHTML += '<div class="song-downloaded">\n' +
+        '                       <div class="image"></div>' +
+        '                       <div class="title-artist">' +
+                '<div class="title"><p>' + element["track"]["name"] + '</p></div>' +
+                '<div class="artist"><p>' + element["track"]['artists'][0]["name"] + '</p></div>' +
+                '</div>' +
+                '<div class="add-to-playlist"><img src="../static/images/Playlist.svg" alt="Add to playlist"></div>' +
         '                   </div>\n'
         )
     });
