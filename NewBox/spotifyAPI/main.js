27166aa779a3809bb7986cd295057d4700ea56ca
Sylvana <56027003+SylvanaStenden@users.noreply.github.com>
2022-05-24 10:19:15 +0200

    Tried adding intercepting

--- NewBox/spotifyAPI/main.js
+++ NewBox/spotifyAPI/main.js
@@ -3,13 +3,21 @@ import axios from "axios";
 //POST REQUEST
 
 function getArtist() {
-    axios({
-        method: 'post',
-        url: 'https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-artist'
-    })
+    axios.post('https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-artist'), {
+        name: ['name'],
+        uri: ['uri']
+    }
         .then(res => showOutput(res))
         .catch(err => console.error(err));
 }
 
 <script src="https://cdnjs.cloudfare.com/ajax/libs/axios/0.19.0/axios.min.js\"></script>
 
+//INTERCEPTING
+
+axios.interceptors.request.use(config => {
+    console.log(`${config.method.toUpperCase()}request sent to ${config.url} at ${new Date().getTime()}`);
+    return config
+}, error => {
+    return Promise.reject(error)
+});
