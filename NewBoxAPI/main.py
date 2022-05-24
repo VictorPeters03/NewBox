746ac6ab7e0686635644a032fb9fac9abf5d36e9
thomm <thom.neijmeijer1@student.nhlstenden.com>
2022-05-24 16:21:58 +0200

    Search and searchall updated and differentiated

--- NewBoxAPI/main.py
+++ NewBoxAPI/main.py
@@ -49,8 +49,16 @@ async def get_queue():
 # endpoint for playing songs
 @app.get("/use/play/{id}")
 async def play_music(id: str):
-    # needs an if statement added to check if the queue is empty
+    return
 
+# endpoint to toggle the state of the current song
+@app.put("/use/toggleplay")
+async def toggle_music():
+    return
+
+# endpoint for searching individual songs in the local database
+@app.get("/use/search/{key}")
+async def search_music(id: str):
     # sets up a connection to the database
     try:
         db = MySQLdb.connect("127.0.0.1", "root", "", "djangosearchbartest")
@@ -60,12 +68,12 @@ async def play_music(id: str):
     cursor = db.cursor()
 
     # the SQL statement
-    sql = "SELECT * FROM `core_song`;"
-    # song_id = id
-    #  WHERE id = %s
+    sql = "SELECT * FROM `core_song` WHERE id = %s;"
+    song_id = id
+
     # executes the statement
-    cursor.execute(sql)
-    # , song_id
+    cursor.execute(sql, song_id)
+
     # takes the data from the statement and places it in a variable
     songs = cursor.fetchall()
 
@@ -78,16 +86,34 @@ async def play_music(id: str):
 
     return dictionary
 
-# endpoint to toggle the state of the current song
-@app.put("/use/toggleplay")
-async def toggle_music():
-    return
+# endpoint for getting all songs
+@app.get("use/searchall/{key}")
+async def search_all(key: str):
+    # sets up a connection to the database
+    try:
+        db = MySQLdb.connect("127.0.0.1", "root", "", "djangosearchbartest")
+    except:
+        return "Can't connect to database"
 
-# endpoint for searching songs in the local database
-@app.get("/use/search/{key}")
-async def search_music(key: str):
-    return
+    cursor = db.cursor()
 
+    # the SQL statement
+    sql = "SELECT * FROM `core_song`;"
+
+    # executes the statement
+    cursor.execute(sql)
+
+    # takes the data from the statement and places it in a variable
+    songs = cursor.fetchall()
+
+    db.close()
+
+    dictionary = []
+
+    for song in songs:
+        dictionary.append({"id": song[0], "artist": song[1], "title": song[2]})
+
+    return dictionary
 
 # endpoint for getting the ip off the rpi
 @app.get("/adminpanel/ip")
