2c3f6eb8be56b86eab694bd37fd083921ec49bb4
Noizeisdead <noizee829@gmail.com>
2022-06-02 09:01:01 +0200

    Test.ino works on the led strip now

--- LedLightArduino/Test.ino
+++ LedLightArduino/Test.ino
@@ -1,17 +1,32 @@
 #include <Adafruit_NeoPixel.h>
-#include <avr/power.h>
-#define PIN            6
 
-#define NUMPIXELS      14
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int delayval = 500;
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 6, NEO_GRB + NEO_KHZ800);
+
 void setup() {
-  pixels.begin();
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
+
 void loop() {
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(255,0,0)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+  // pixels: rgb => brg
+  colorWipe(strip.Color(50, 255, 0), 50); // #690063
+  colorWipe(strip.Color(250, 230, 0), 50); // #7B001A
+  colorWipe(strip.Color(255, 0, 0), 50); // #08002E
+  colorWipe(strip.Color(25, 255, 0), 50); // #530055
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
   }
 }
