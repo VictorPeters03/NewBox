5bebe0b9dfb5618dff4244831b564b7eda5e13d9
thomm <thom.neijmeijer1@student.nhlstenden.com>
2022-06-20 10:13:00 +0200

    reload added to local api script

--- local_API.sh
+++ local_API.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # move this file to /etc/profile.d/
 cd /home/newbox/NewBox/NewBoxAPI
-/usr/local/bin/uvicorn main:app --host 0.0.0.0 --root-path /home/newbox/NewBox/NewBoxAPI &
+/usr/local/bin/uvicorn main:app --host 0.0.0.0 --reload --root-path /home/newbox/NewBox/NewBoxAPI &
