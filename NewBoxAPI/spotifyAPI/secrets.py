a1b86adcecb33fd1f00b6d4c34c3c91b5c3155b9
thomm <thom.neijmeijer1@student.nhlstenden.com>
2022-06-20 09:11:04 +0200

    Hardcoded spotify client id because now it at least works

--- NewBoxAPI/spotifyAPI/secrets.py
+++ NewBoxAPI/spotifyAPI/secrets.py
@@ -1,8 +1,8 @@
 import os
 # spotify_client_id and spotify_client_secret must be set as environment variables on own pc
 # these can be found by logging in to developer.spotify.com -> dashboard -> copy codes and add them as envs
-SPOTIFY_CLIENT_ID = os.environ.get('spotify_client_id')
-SPOTIFY_CLIENT_SECRET = os.environ.get('spotify_client_secret')
+SPOTIFY_CLIENT_ID = '99117380ca794cdeadaa05089d88c79d'
+SPOTIFY_CLIENT_SECRET = 'f6d7ae770020431ab0b8a9190822d41f'
 SPOTIFY_REDIRECT_URI = 'http://localhost:8080'
 # DEVICE_ID = getDevices()
 USER = '31dpnolbrnlodprs3cph3m5izary'
