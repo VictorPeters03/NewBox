0fffe41bf992dd8a80578d7a73f0b54896f1dbfc
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2022-06-22 13:20:53 +0200

    made login page for adminpanel and is now working

--- NewBox/NewBox/settings.py
+++ NewBox/NewBox/settings.py
@@ -124,4 +124,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoFielsd'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
