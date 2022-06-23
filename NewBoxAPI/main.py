d4d20ca4e35bf882b54348e429822ff58cddfc41
Noizeisdead <noizee829@gmail.com>
2022-06-23 13:42:25 +0200

    uncomented imports

--- NewBoxAPI/main.py
+++ NewBoxAPI/main.py
@@ -9,12 +9,12 @@ from time import sleep
 import vlc
 import asyncio
 import player
-# import alsaaudio
-# from colorthief import ColorThief
-# from colormap import hex2rgb
-# import serial
-# from urllib.request import urlretrieve
-# import urllib3
+import alsaaudio
+from colorthief import ColorThief
+from colormap import hex2rgb
+import serial
+from urllib.request import urlretrieve
+import urllib3
 
 app = FastAPI()
 
